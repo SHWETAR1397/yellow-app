aae03986ac0a8cc76008d6aa571b77f3fbe5c001
koderoom <koderoom.dev@gmail.com>
2020-09-01 12:38:42 +0530

    how.to.add.icons.

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faApple, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
 })
 export class Page1Component implements OnInit {
+  public title = 'Hello World';
+  public faApple = faApple;
+  public faGoogle = faGoogle;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
