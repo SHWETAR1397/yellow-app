e78eb5e14ef7692b0c368dbea7f43adb18f3caf8
koderoom <koderoom.dev@gmail.com>
2020-09-01 11:57:54 +0530

    page2.page3.added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
